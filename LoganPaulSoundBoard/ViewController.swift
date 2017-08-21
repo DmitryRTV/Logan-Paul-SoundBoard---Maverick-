8911249e258f6cf10c0017d66123996e2ac8e87d
Dmitri Marian <dimamarian61@gmail.com>
2017-08-21 09:41:24 -0700

    new commit

---
+++ LoganPaulSoundBoard/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LoganPaulSoundBoard
+//
+//  Created by Dmitri Marian on 8/19/17.
+//  Copyright © 2017 DmitryRTV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
